72093f60b888f4b616f170d1f640076afc783602
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-12-30 13:46:03 -0500

    se elimina el proyecto del repo.

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const detalle = document.getElementById('uno');
-const option = document.getElementById('dos');
-const valor = document.getElementById('tres');
-
-const btnEvent = document.getElementById('btn-submit');
-
-btnEvent.addEventListener('click', console.log(`${detalle.value}, ${option.value}, ${valor.value}`));
