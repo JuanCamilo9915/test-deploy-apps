6d0d6b1a6604f0b752f428956e3ab0bca3cc326d
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-12-31 15:33:25 -0500

    se realizan pequeñas modificaciones en los archivos: webpack.config.js, index.ts y db.ts .

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,16 @@ const path = require('path');
 module.exports = {
     mode: 'production',
     entry: './src/index.ts',
+    //entry: './src/db.ts',
     module: {
         rules: [{
             test: /\.ts$/,
             use: 'ts-loader',
             include: [path.resolve(__dirname, 'src')]
         }]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
     },
     output: {
         filename: 'main.js',
