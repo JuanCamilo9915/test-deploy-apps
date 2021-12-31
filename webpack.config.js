ff0c2fae39488b0eef60e9b5da482d26ce9011b3
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-12-31 12:56:06 -0500

    se realiza pequeño cambio en las tabulaciones del archivo webpack.config.js .

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-  entry: './src/index.ts',
-  module: {
-      rules: [{
-          test: /\.ts$/,
-          use: 'ts-loader',
-          include: [path.resolve(__dirname, 'src')]
-      }]
-  },
-  output: {
-    filename: 'main.js',
-    //filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
+    entry: './src/index.ts',
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            include: [path.resolve(__dirname, 'src')]
+        }]
+    },
+    output: {
+        filename: 'main.js',
+        //filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
 };
